521b87d540a30809967a6f90c79cd4719da683b9
IGSysytemI <assassin.vipq@gmail.com>
2021-05-18 12:56:32 +0200

    lab8

---
+++ CCL/Security/Identity/Doctor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSBB.Security.Identity
+{
+    public class Doctor
+        : Client
+    {
+        public Doctor(int userId, string name, int catalogId) 
+            : base(userId, name, catalogId, nameof(Doctor))
+        {
+        }
+    }
+}
